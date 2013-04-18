60029518e12fc53ee41d7d84d179882b0ac883ae
Stephen Sykes <sdsykes@gmail.com>
2013-04-18 15:55:01 +0300

    Revised url for pennysmalls reference, posterous is shutting down shortly.

--- lib/fastimage.rb
+++ lib/fastimage.rb
@@ -35,7 +35,7 @@
 # === References
 # * http://snippets.dzone.com/posts/show/805
 # * http://www.anttikupila.com/flash/getting-jpg-dimensions-with-as3-without-loading-the-entire-file/
-# * http://pennysmalls.com/find-jpeg-dimensions-fast-in-pure-ruby-no-ima
+# * http://pennysmalls.wordpress.com/2008/08/19/find-jpeg-dimensions-fast-in-pure-ruby-no-ima/
 # * http://imagesize.rubyforge.org/
 #
 
