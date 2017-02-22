6bd210f3d99f08723aef76fa1ec2eeeb63890877
John Shearar <john.shearar@gmail.com>
2017-02-22 13:14:59 +0200

    timeouts seem to raise 'IOError: closed stream' on my machine

--- lib/fastimage.rb
+++ lib/fastimage.rb
@@ -201,7 +201,7 @@ class FastImage
     raise SizeNotFound if @options[:raise_on_failure] && @property == :size && !@size
 
   rescue Timeout::Error, SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ECONNRESET,
-    ImageFetchFailure, Net::HTTPBadResponse, EOFError, Errno::ENOENT
+    ImageFetchFailure, Net::HTTPBadResponse, EOFError, Errno::ENOENT, IOError
     raise ImageFetchFailure if @options[:raise_on_failure]
   rescue NoMethodError  # 1.8.7p248 can raise this due to a net/http bug
     raise ImageFetchFailure if @options[:raise_on_failure]
